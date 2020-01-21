c6579deb9676ee63387044d4ffc5716995376cb4
NemesAlexandru <alexandrunemes1@gmail.com>
2020-01-22 01:33:51 +0200

    Single product display

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -7,7 +7,7 @@ window.singleProduct = {
             method: "GET"
         }).done(function (response) {
             console.log(response);
-            singleProduct.displayProduct(response.content)
+            singleProduct.displayProduct(response)
         });
 
     },
@@ -27,18 +27,15 @@ window.singleProduct = {
                                 <div class="product-inner-price">
                                     <ins>$700.00</ins> <del>$800.00</del>
                                 </div>
-
                                 <form action="" class="cart">
                                     <div class="quantity">
                                         <input type="number" size="4" class="input-text qty text" title="Qty" value="1" name="quantity" min="1" step="1">
                                     </div>
                                     <button class="add_to_cart_button" type="submit">Add to cart</button>
                                 </form>
-
                                 <div class="product-inner-category">
                                     <p>Category: <a href="">Summer</a>. Tags: <a href="">awesome</a>, <a href="">best</a>, <a href="">sale</a>, <a href="">shoes</a>. </p>
                                 </div>
-
                                 <div role="tabpanel">
                                     <ul class="product-tab" role="tablist">
                                         <li role="presentation" class="active"><a href="#home" aria-controls="home" role="tab" data-toggle="tab">Description</a></li>
@@ -48,7 +45,6 @@ window.singleProduct = {
                                         <div role="tabpanel" class="tab-pane fade in active" id="home">
                                             <h2>Product Description</h2>
                                             <p>Alex Nemes cel mai tare</p>
-
                                             <p>Cocosi pe locul 2</p>
                                         </div>
                                         <div role="tabpanel" class="tab-pane fade" id="profile">
@@ -58,7 +54,6 @@ window.singleProduct = {
                                                 <p><label for="email">Email</label> <input name="email" type="email"></p>
                                                 <div class="rating-chooser">
                                                     <p>Your rating</p>
-
                                                     <div class="rating-wrap-post">
                                                         <i class="fa fa-star"></i>
                                                         <i class="fa fa-star"></i>
@@ -73,7 +68,6 @@ window.singleProduct = {
                                         </div>
                                     </div>
                                 </div>
-
                             </div>
                         </div>`
 
