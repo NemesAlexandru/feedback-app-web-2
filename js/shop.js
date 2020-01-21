8aac7ce879f05eac178c188e92a40154ca21205b
NemesAlexandru <alexandrunemes1@gmail.com>
2020-01-21 19:37:22 +0200

    Initial commit

---
+++ js/shop.js
@@ -0,0 +1,63 @@
+window.Shop = {
+    API_BASE_URL: "http://localhost:8085",
+
+    getProducts: function () {
+        $.ajax({
+            url: Shop.API_BASE_URL + "/products"
+            //default ajax method: "GET"
+        }).done(function (response) {
+            console.log(response);
+
+            Shop.displayProducts(response.content);
+        });
+    },
+
+    redirectToSingleProduct: function (productId)  {
+        $.ajax({
+            url: Shop.API_BASE_URL + "/products/" + productId,
+            method: "GET",
+            // contentType: "application/json",
+            // data: JSON.stringify(request)
+        }).done(function () {
+            window.location.replace("single-product.html");
+            singleProduct.getProduct(productId)
+        })
+
+    },
+
+    bindEvents: function () {
+        $(".single-product-area").delegate(".add_to_cart_button", "click", function (event) {
+            event.preventDefault();
+            let productId = $(this).data("product_id");
+            Shop.redirectToSingleProduct(productId);
+        })
+    },
+
+    getProductsHtml: function (product) {
+        return `<div class="col-md-3 col-sm-6">
+                    <div class="single-shop-product">
+                        <div class="product-upper">
+                            <img src="img/product-2.jpg" alt="">
+                        </div>
+                        <h2><a href="">${product.name}</a></h2>
+                        <div class="product-carousel-price">
+                            <ins>$899.00</ins> <del>$999.00</del>
+                        </div>  
+                        
+                        <div class="product-option-shop">
+                            <a class="add_to_cart_button" data-quantity="1" data-product_sku="" data-product_id=${product.id} rel="nofollow" href="/canvas/shop/?add-to-cart=${product.id}">Add Review</a>
+                        </div>                       
+                    </div>
+                </div>`
+    },
+
+    displayProducts: function (products) {
+        let productsHtml = "";
+
+        products.forEach(oneProduct => productsHtml += Shop.getProductsHtml(oneProduct));
+        $(".single-product-area .row:first-child").html(productsHtml);
+    }
+};
+
+Shop.getProducts();
+Shop.bindEvents();
