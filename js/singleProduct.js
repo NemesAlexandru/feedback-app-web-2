49c777db9b4ca2e25b2ac32463aae838d0f299e1
NemesAlexandru <alexandrunemes1@gmail.com>
2020-01-21 23:43:42 +0200

    singleProduct GET not hitting API (hard coding productId)

--- js/singleProduct.js
+++ js/singleProduct.js
@@ -15,14 +15,14 @@ window.singleProduct = {
     displayProduct: function (product) {
         let productHtml;
         productHtml = singleProduct.getSingleProductHtml(product);
-        $(".col-sm-6:first-child").html(productHtml);
-
+        $(".col-md-8 .row:nth-child(2)").html(productHtml);
     },
 
 
     getSingleProductHtml: function (product) {
 
-        return `<div class="product-inner">
+        return `<div class="col-sm-6">
+                            <div class="product-inner">
                                 <h2 class="product-name">${product.name}</h2>
                                 <div class="product-inner-price">
                                     <ins>$700.00</ins> <del>$800.00</del>
@@ -32,7 +32,7 @@ window.singleProduct = {
                                     <div class="quantity">
                                         <input type="number" size="4" class="input-text qty text" title="Qty" value="1" name="quantity" min="1" step="1">
                                     </div>
-                                    <button class="add_to_cart_button" type="submit">Add Review</button>
+                                    <button class="add_to_cart_button" type="submit">Add to cart</button>
                                 </form>
 
                                 <div class="product-inner-category">
@@ -47,9 +47,9 @@ window.singleProduct = {
                                     <div class="tab-content">
                                         <div role="tabpanel" class="tab-pane fade in active" id="home">
                                             <h2>Product Description</h2>
-                                            <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam tristique, diam in consequat iaculis, est purus iaculis mauris, imperdiet facilisis ante ligula at nulla. Quisque volutpat nulla risus, id maximus ex aliquet ut. Suspendisse potenti. Nulla varius lectus id turpis dignissim porta. Quisque magna arcu, blandit quis felis vehicula, feugiat gravida diam. Nullam nec turpis ligula. Aliquam quis blandit elit, ac sodales nisl. Aliquam eget dolor eget elit malesuada aliquet. In varius lorem lorem, semper bibendum lectus lobortis ac.</p>
+                                            <p>Alex Nemes cel mai tare</p>
 
-                                            <p>Mauris placerat vitae lorem gravida viverra. Mauris in fringilla ex. Nulla facilisi. Etiam scelerisque tincidunt quam facilisis lobortis. In malesuada pulvinar neque a consectetur. Nunc aliquam gravida purus, non malesuada sem accumsan in. Morbi vel sodales libero.</p>
+                                            <p>Cocosi pe locul 2</p>
                                         </div>
                                         <div role="tabpanel" class="tab-pane fade" id="profile">
                                             <h2>Reviews</h2>
@@ -66,9 +66,18 @@ window.singleProduct = {
                                                         <i class="fa fa-star"></i>
                                                         <i class="fa fa-star"></i>
                                                     </div>
-                                                </div>`
+                                                </div>
+                                                <p><label for="review">Your review</label> <textarea name="review" id="" cols="30" rows="10"></textarea></p>
+                                                <p><input type="submit" value="Submit"></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>`
 
     },
 
 };
-singleProduct.getProduct();
+singleProduct.getProduct(5);
